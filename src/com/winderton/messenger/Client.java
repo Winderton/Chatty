31cfaf430d161e669f4acf65c60a064b1a2f0240
Winderton <recursi0n.edu@gmail.com>
2016-08-14 12:08:31 +0300

    Initial commit

---
+++ src/com/winderton/messenger/Client.java
@@ -0,0 +1,119 @@
+package com.winderton.messenger;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+ 
+ public class Client extends JFrame {
+ 	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+ 
+ 	private String name, address;
+ 	private int port;
+	private JTextField txtMessage;
+	private JTextArea history;
+ 	
+ 	public Client(String name, String address, int port) {
+ 		setTitle("Messenger Client");
+		this.name = name;
+		this.address = address;
+		this.port = port;
+		createWindow();
+		console("Пытаюсь подключиться к: " + address + " на " + port + " порт, пользователь: " + name );
+ 	}
+ 
+ 	private void createWindow() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+ 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 		setSize(880, 550);
+ 		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+ 		setContentPane(contentPane);
+ 		
+ 		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{16, 857, 7};
+		gbl_contentPane.columnWidths = new int[]{16, 827, 30, 7};
+ 		gbl_contentPane.rowHeights = new int[]{35, 475, 40};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
+ 		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+ 		contentPane.setLayout(gbl_contentPane);
+ 		
+ 		history = new JTextArea();
+		history.setEditable(false);
+		JScrollPane scroll = new JScrollPane(history);
+ 		GridBagConstraints scrollConstraints = new GridBagConstraints();
+		scrollConstraints.insets = new Insets(0, 0, 5, 5);
+ 		scrollConstraints.fill = GridBagConstraints.BOTH;
+ 		scrollConstraints.gridx = 0;
+ 		scrollConstraints.gridy = 0;
+		scrollConstraints.gridwidth = 3;
+		scrollConstraints.gridheight = 2;
+		scrollConstraints.insets = new Insets(0, 7, 0, 0);
+ 		contentPane.add(scroll, scrollConstraints);
+		
+		txtMessage = new JTextField();
+		txtMessage.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					send(txtMessage.getText());
+				}
+			}
+		});
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 0;
+		gbc_txtMessage.gridy = 2;
+		gbc_txtMessage.gridwidth = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				send(txtMessage.getText());
+			}
+		});
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		contentPane.add(btnSend, gbc_btnSend);
+ 		setVisible(true);
+ 		
+ 		txtMessage.requestFocusInWindow();
+ 	}
+ 	
+ 	public void send(String message) {
+ 		if (message.equals("")) return;
+ 		message = name + ": " + message;
+		console(message);
+		txtMessage.setText("");
+ 	}
+ 	
+ 	public void console(String message) {
+ 		history.setCaretPosition(history.getDocument().getLength());
+ 		history.append(message + "\n\r");
+ 	}
+ }
---
+++ src/com/winderton/messenger/Login.java
@@ -0,0 +1,111 @@
+package com.winderton.messenger;
+
+import java.awt.EventQueue;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	private JTextField txtAddress;
+	private JTextField txtPort;
+	private JTextField txtName;
+	private JLabel lblIpAddress;
+	private JLabel lblPort;
+	private JLabel lblAddressDesc;
+	private JLabel lblPortDesc;
+		
+	public Login() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 380);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(79, 60, 142, 26);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(119, 45, 61, 16);
+		contentPane.add(lblName);
+		
+		lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setBounds(111, 98, 78, 16);
+		contentPane.add(lblIpAddress);
+		
+		txtAddress = new JTextField();
+		txtAddress.setBounds(79, 114, 142, 26);
+		contentPane.add(txtAddress);
+		txtAddress.setColumns(10);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(79, 184, 142, 26);
+		contentPane.add(txtPort);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(130, 168, 40, 16);
+		contentPane.add(lblPort);
+		
+		lblAddressDesc = new JLabel("(пр. 192.168.0.2)");
+		lblAddressDesc.setBounds(95, 140, 110, 16);
+		contentPane.add(lblAddressDesc);
+		
+		lblPortDesc = new JLabel("(пр. 8192)");
+		lblPortDesc.setBounds(115, 209, 70, 16);
+		contentPane.add(lblPortDesc);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String name = txtName.getText();
+				String address = txtAddress.getText();
+				int port = Integer.parseInt(txtPort.getText());
+				login(name, address, port);
+			}
+		});
+		btnLogin.setBounds(88, 300, 117, 29);
+		contentPane.add(btnLogin);
+	}
+	
+	private void login(String name, String address, int port) {
+		dispose();
+		new Client(name, address, port);
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
